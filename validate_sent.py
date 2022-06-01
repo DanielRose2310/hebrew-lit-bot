7942b872ee4fa2115ae5c50c380a47de1f8f25af
DanielRose <dddrrr84@gmail.com>
2022-06-01 22:45:31 +0300

    .

--- validate_sent.py
+++ validate_sent.py
@@ -3,7 +3,7 @@ import re
 closers = ['.','?','!','...']
 
 def count_chars(s,chars):
-    return {c : s.count(c) for c in chars}
+    return sum([s.count(c) for c in chars])
 
 def sent_good(sent):
     if  sent[-1] in closers and count_chars(sent,closers)>1 and not sent.count('"')%2 and not len(re.findall('\(|\)', sent))%2:
