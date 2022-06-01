30658b4319ea7476e38681d2619c4b8d30c796b7
DanielRose <dddrrr84@gmail.com>
2022-06-01 23:21:44 +0300

    .

--- validate_sent.py
+++ validate_sent.py
@@ -8,5 +8,7 @@ def count_chars(s,chars):
 def sent_good(sent):
     if  sent[-1] in closers and count_chars(sent,closers)>1 and not sent.count('"')%2 and not len(re.findall('\(|\)', sent))%2:
         return True
+    elif sent[-1] in closers and len(sent)>40:
+        return True
     else:
         return False
