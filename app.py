71fa3a39e98216caa0494a6876b88d73e26ebf78
DanielRose <dddrrr84@gmail.com>
2022-06-01 20:30:02 +0300

    .

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
+from flask import jsonify
+
 app = Flask(__name__)
 CORS(app)
 from generate_text import generate_text
@@ -10,6 +12,6 @@ def hello_world(name):
     if name in names or name is None:
         model_from_file = open("models/"+name+".pkl", "rb")
         model_from_file = pickle.load(model_from_file)
-        return generate_text(model_from_file)
+        return jsonify(generate_text(model_from_file))
     else:
         return "Invalid param!"
