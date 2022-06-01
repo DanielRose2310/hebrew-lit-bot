8872850de5d12465b364175d812bd1fd8d8f769a
DanielRose <dddrrr84@gmail.com>
2022-06-01 22:20:40 +0300

    .

--- generate_text.py
+++ generate_text.py
@@ -3,11 +3,8 @@ from string import punctuation
 import re 
 from validate_sent import sent_good
 
-def generate_text(model, state_size=2, min_length=10):
-    '''
-    Consume a Markov chain model (make sure to specify the <state_size> used)
-    to generate text that is at least <min_length> size long.
-    '''
+def generate_text(model, state_size=2):
+
     def get_new_starter():
         return random.choice([s.split(' ') for s in model.keys() if not any(p in s for p in punctuation)])
     text = get_new_starter()
@@ -25,6 +22,6 @@ def generate_text(model, state_size=2, min_length=10):
             next_word = random.choice(model[key])
         text.append(next_word)
         i += 1
-        if i > min_length and text[-1][-1] == '.' and sent_good(' '.join(text)):
+        if sent_good(' '.join(text)):
             break
     return ' '.join(text)
