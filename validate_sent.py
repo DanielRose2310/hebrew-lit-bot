1afc975786e1beba9e2a59b0aa07c9e3001f934c
DanielRose <dddrrr84@gmail.com>
2022-06-01 22:34:07 +0300

    .

--- validate_sent.py
+++ validate_sent.py
@@ -1,7 +1,7 @@
 import re
 closers = ['.','?','!']
 def sent_good(sent):
-    if  sent[-1] in closers and not sent.count('"')%2 and not len(re.findall('\(|\)', sent))%2:
+    if  sent[-1] in closers and len(sent>10) and not sent.count('"')%2 and not len(re.findall('\(|\)', sent))%2:
         return True
     else:
         return False
